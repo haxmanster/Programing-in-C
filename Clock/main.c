28fc47e6035c70b6983f9f2d4b0e17ae389b8b4e
Wolyniec Grzegorz <grzegorz.wolyniec@tieto.com>
2018-07-24 12:02:16 +0200

    Create First program in C Language Clock Ofcourse :D

---
+++ Clock/main.c
@@ -0,0 +1,13 @@
+
+int main(){
+    int godzina,minuta;
+    int wynik;
+    printf("Input your time to calculate clock angle ");
+    printf("\nInput Hour:");
+    scanf("%d",&godzina,&minuta);
+    printf("Input minutes:");
+    scanf("%d",&minuta);
+    wynik = ((5.5 * minuta) - (30 * godzina));
+    printf("Angle is %d", abs(wynik));
+    return 0;
+}
