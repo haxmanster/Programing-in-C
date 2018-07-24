e81f5ad978355a8547767bd365f2572134210c58
Wolyniec Grzegorz <grzegorz.wolyniec@tieto.com>
2018-07-24 12:04:37 +0200

    Remove double Variable &minuta

--- Clock/main.c
+++ Clock/main.c
@@ -4,7 +4,7 @@ int main(){
     int wynik;
     printf("Input your time to calculate clock angle ");
     printf("\nInput Hour:");
-    scanf("%d",&godzina,&minuta);
+    scanf("%d",&godzina);
     printf("Input minutes:");
     scanf("%d",&minuta);
     wynik = ((5.5 * minuta) - (30 * godzina));
