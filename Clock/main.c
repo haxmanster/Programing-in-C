be52e85e7e7a7d77b33a8518045e21719897aeae
Wolyniec Grzegorz <grzegorz.wolyniec@tieto.com>
2018-07-24 13:18:38 +0200

    While loop :D

--- Clock/main.c
+++ Clock/main.c
@@ -1,13 +1,19 @@
 
 int main(){
-    int godzina,minuta;
-    int wynik;
-    printf("Input your time to calculate clock angle ");
-    printf("\nInput Hour:");
-    scanf("%d",&godzina);
-    printf("Input minutes:");
-    scanf("%d",&minuta);
-    wynik = ((5.5 * minuta) - (30 * godzina));
-    printf("Angle is %d", abs(wynik));
+    char exit = 'n';
+    while (exit != 'y')
+        {
+        int godzina,minuta;
+        int wynik;
+        printf("\nInput your time to calculate clock angle ");
+        printf("\nInput Hour:");
+        scanf("%d",&godzina);
+        printf("Input minutes:");
+        scanf("%d",&minuta);
+        wynik = ((5.5 * minuta) - (30 * godzina));
+        printf("Angle is %d", abs(wynik));
+        printf("\nExit y/n ?");
+        scanf("%s", &exit);
+        }
     return 0;
 }
